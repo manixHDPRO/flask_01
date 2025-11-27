db60db6c9c533c84635d162e5f6c9386bf797792
Manix Mayinda <manixmayinda11@gmail.com>
2025-11-27 05:43:50 +0100

    Partie 02 route

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Bonjour, le monde !</h1>'
+def home():
+    return 'Salut HD PRO ! C\'est ma première application Flask.'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return 'À propos de cette application Flask.'
+
+@app.route('/contact')
+def contact():
+    return 'Ceci est une page de contact.'
+
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    if request.method == 'POST':
+        return 'Vous envoyez des données !'
+    else:
+        return 'Vous visualisez uniquement le formulaire'
+
